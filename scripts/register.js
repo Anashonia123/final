19bd4b1d5cb85918a0d9d4a7b8fe4bc01a95fd53
Anashonia123 <anashonia098@gmail.com>
2024-03-14 21:47:45 +0400

    add

---
+++ scripts/register.js
@@ -0,0 +1,20 @@
+let signup = document.querySelector(".signup");
+let login = document.querySelector(".login");
+let slider = document.querySelector(".slider");
+let formSection = document.querySelector(".form-section");
+ 
+signup.addEventListener("click", () => {
+    slider.classList.add("moveslider");
+    formSection.classList.add("form-section-move");
+});
+ 
+login.addEventListener("click", () => {
+    slider.classList.remove("moveslider");
+    formSection.classList.remove("form-section-move");
+});
+// if (username.trim() === '' || email.trim() === '' || password.trim() === '') {
+//     alert("Please fill in all fields");
+//     return;
+// }
+// alert("Registration successful!");
+// window.location.href = "index.html";
